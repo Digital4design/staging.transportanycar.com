24559db8eb9e3142ce0c7a6f0b28f9b9567ff153
kssharma22 <ks.sharma0111@gmail.com>
2024-10-24 11:41:55 +0530

    done

--- resources/views/front/get_steps.blade.php
+++ resources/views/front/get_steps.blade.php
@@ -472,13 +472,10 @@
                                         </span>
                                         <input type="text" class="form-control wizard-required email" placeholder="Enter your phone" name="phone" value="" />
                                     </div>
-                                    {{-- <small class="exm_text d-block">e.g. abc@gmail.com</small> --}}
+                                   
                                 </div>
                             <div class="form-group wizard_btngrp">
-                                {{--<a href="getsteps.html" class="wd_nextbtn form-wizard-submit">Get Instant Quotes <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1.4848 1.62598L8.35854 8.49971L1.4848 15.3734" stroke="white" stroke-width="2.06212" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </a>--}}
+                               
                                 <button type="submit" class="wd_nextbtn form-wizard-submit" id="SaveAccount">Get Instant Quotes <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M1.4848 1.62598L8.35854 8.49971L1.4848 15.3734" stroke="white" stroke-width="2.06212" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
