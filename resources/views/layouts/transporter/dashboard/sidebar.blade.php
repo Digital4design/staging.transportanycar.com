c1edc50f68b0360581d392850e8064ec6f87c630
ravid4d <ravichaudharyd4d@gmail.com>
2024-11-22 13:58:56 +0530

    make functionality to check transporter can't bid if he has not filled company details

--- resources/views/layouts/transporter/dashboard/sidebar.blade.php
+++ resources/views/layouts/transporter/dashboard/sidebar.blade.php
@@ -380,11 +380,24 @@ $currentRoute = request()->route()->getName();
             var driver_license = "{{ Auth::user()->driver_license }}";
             var goods_in_transit_insurance = "{{ Auth::user()->goods_in_transit_insurance }}";
 
+            {{-- var companyDetails = "{{ Auth::user()->companyDetail}}";
+            var git_insurance_cover = "{{ Auth::user()->companyDetail->git_insurance_cover ?? null}}";
+            var years_established = "{{ Auth::user()->companyDetail->years_established ?? null}}";
+            var no_of_tow_trucks = "{{ Auth::user()->companyDetail->no_of_tow_trucks ?? null}}";
+            var no_of_drivers  = "{{ Auth::user()->companyDetail->no_of_drivers ?? null}}"; --}}
+
+            var companyDetails = @json(optional(Auth::user()->companyDetail)->id);
+            var git_insurance_cover = @json(optional(Auth::user()->companyDetail)->git_insurance_cover ?? null);
+            var years_established = @json(optional(Auth::user()->companyDetail)->years_established ?? null);
+            var no_of_tow_trucks = @json(optional(Auth::user()->companyDetail)->no_of_tow_trucks ?? null);
+            var no_of_drivers = @json(optional(Auth::user()->companyDetail)->no_of_drivers ?? null);
+            console.log(companyDetails, git_insurance_cover, years_established, no_of_tow_trucks, no_of_drivers);
+
             if (driver_license == null && goods_in_transit_insurance == null && email_status == '0') {
                 e.preventDefault();
                 Swal.fire({
                     title: '<span class="swal-title" style="color:#ED1C24">Verify your account</span>',
-                    html: '<span class="swal-text">  You must upload a valid drivers license, goods in transit insurance and verify your email address within your profile before you are able to bid for jobs.</span>',
+                    html: '<span class="swal-text"> You must upload your drivers license,goods in transit insurance,verify your email and complete your company details within your profile before you are able to bid for jobs.</span>',
                     confirmButtonColor: '#52D017',
                     confirmButtonText: 'Verify',
                     customClass: {
@@ -450,7 +463,33 @@ $currentRoute = request()->route()->getName();
                     }
                 });
 
-            } else if (isStatus == 'pending' || isStatus == 'rejected') {
+            }
+            else if(companyDetails === null || git_insurance_cover === null || years_established === null || no_of_tow_trucks === null || no_of_drivers === null)
+            {
+                console.log("hello Errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+                Swal.fire({
+                    title: '<span class="swal-title" style="color:#ED1C24">Verify your account</span>',
+                    html: '<span class="swal-text"> You must upload your drivers license,goods in transit insurance,verify your email and complete your company details within your profile before you are able to bid for jobs.</span>',
+                    confirmButtonColor: '#52D017',
+                    confirmButtonText: 'Verify',
+                    customClass: {
+                        title: 'swal-title',
+                        htmlContainer: 'swal-text-container',
+                        popup: 'swal-popup', // Add custom class for the popup
+                        cancelButton: 'swal-button--cancel' // Add custom class for the cancel button
+                    },
+                    showCloseButton: true, // Add this line to show the close button
+                    showConfirmButton: true, // Add this line to hide the confirm button
+                    allowOutsideClick: false
+                }).then((result) => {
+                    // Redirect to the dashboard if the confirm button or close button is clicked
+                    if (result.isConfirmed || result.dismiss) {
+                        window.location.href =
+                        "{{ route('transporter.profile') }}"; // Change this to your actual dashboard URL
+                    }
+                });
+            } 
+            else if (isStatus == 'pending' || isStatus == 'rejected') {
                 e.preventDefault();
                 Swal.fire({
                     title: '<span class="swal-title" style="color:#ED1C24">Awaiting Approval</span>',
