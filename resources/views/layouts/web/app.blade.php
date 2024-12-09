349d1831869896cd91d106fb650e5f0f0d5c6555
kssharma22 <ks.sharma0111@gmail.com>
2024-12-09 15:01:12 +0530

    new chnages relist and crom job

--- resources/views/layouts/web/app.blade.php
+++ resources/views/layouts/web/app.blade.php
@@ -21,7 +21,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     
     @yield('head_css')
-  <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16465579063"></script> 
+  {{-- <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16465579063"></script>  --}}
+ <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-572LPM3G');</script>
+  <!-- End Google Tag Manager -->
   <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16465579063'); </script>
   <script>
     (function(w,d,t,r,u)
