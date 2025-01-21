d78cd2481ca7114427bf30e9bb348bb7dc22f458
sandeep kamra <sandeep.d4d@gmail.com>
2025-01-21 12:58:00 +0530

    make modal in center vertically

--- resources/views/transporter/dashboard/job_infromation.blade.php
+++ resources/views/transporter/dashboard/job_infromation.blade.php
@@ -940,7 +940,7 @@
 </div>
 
 <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
