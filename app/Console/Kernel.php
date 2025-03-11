541195d4df2c26820a5cb47901bc4f2238511932
kssharma22 <ks.sharma0111@gmail.com>
2025-03-11 18:23:03 +0530

    quejob set inschedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -49,8 +49,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:test')->everyMinute();
         $schedule->command('send:feedback-reminder')->dailyAt('19:00');
         $schedule->command('queue:work --tries=3 --timeout=180')
-        ->withoutOverlapping()
-        ->runInBackground()
+        // ->withoutOverlapping()
+        // ->runInBackground()
         ->everyMinute();
 
     }
