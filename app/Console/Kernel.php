cd8cba2d6d269f38da4ed23725658ba7c885df8e
kssharma22 <ks.sharma0111@gmail.com>
2025-03-12 12:25:36 +0530

    face data with email work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -48,9 +48,9 @@ class Kernel extends ConsoleKernel
         // $schedule->command('send:save-search-mail')->dailyAt('13:00');
         $schedule->command('command:test')->everyMinute();
         $schedule->command('send:feedback-reminder')->dailyAt('19:00');
-        $schedule->command('queue:work --tries=3 --timeout=180')
-        ->everyMinute()
-        ->name('run-queue-worker');
+        // $schedule->command('queue:work --tries=3 --timeout=180')
+        // ->everyMinute()
+        // ->name('run-queue-worker');
 
     }
 
