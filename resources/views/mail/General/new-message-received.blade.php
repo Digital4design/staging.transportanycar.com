3c07ddb61258b0f38f322be0cae49cc36e089d60
vishwasd4d <vishwasd4d@gmail.com>
2025-06-05 10:43:02 +0530

    testing

--- resources/views/mail/General/new-message-received.blade.php
+++ resources/views/mail/General/new-message-received.blade.php
@@ -116,17 +116,16 @@
                         </span>
                     </a>
                 @else
-                @php
-                    $href = '#'; // Default fallback
-
+               @php
     if (isset($data['main_url']) && $data['main_url'] === route('transporter.job_information')) {
         $href = $data['main_url'] . '/' . $data['quotes']->id . '?scroll=bid_wrapper';
     } elseif (isset($data['quotes']->id)) {
         $userIdPart = isset($data['user']->id) ? '/' . $data['user']->id : '';
-        // $href = url('quotes/' . $data['quotes']->id . $userIdPart);
-          $href = url('transporter/messages');
+        $href = route('transporter.messages', ['thread_id' => $thread_id]); 
+    } else {
+        $href = '#'; 
     }
-                @endphp
+@endphp
                     <a href="{{ $href }}"
                         target="_blank"
                         style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#52d017;border-radius:2px;width:auto;border-top:0px solid #8a3b8f;font-weight:400;border-right:0px solid #8a3b8f;border-bottom:0px solid #8a3b8f;border-left:0px solid #8a3b8f;padding-top:5px;padding-bottom:5px;font-family:'Montserrat', sans-serif;font-size:16px;text-align:center;mso-border-alt:none;word-break:keep-all;">
