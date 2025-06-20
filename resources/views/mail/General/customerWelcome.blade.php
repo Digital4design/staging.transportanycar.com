8be1945240d94dd2ccf85404de27d94f507bea02
vishwasd4d <vishwasd4d@gmail.com>
2025-06-20 11:57:42 +0530

    fixed email typo

--- resources/views/mail/General/customerWelcome.blade.php
+++ resources/views/mail/General/customerWelcome.blade.php
@@ -440,7 +440,7 @@
                                                                         style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:25.5px;color:#666666;font-size:17px">
                                                                         You can compare your quotes and ask questions
                                                                         via message before accepting to make sure you
-                                                                        choose the right one for you, check outyour quotes
+                                                                        choose the right one for you, check out your quotes
                                                                         <a href="{{ url('/dashboard') }}"> here.</a>
                                                                     </p>
                                                                     <p
@@ -448,7 +448,7 @@
                                                                         <br></p>
                                                                     <p
                                                                         style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:25.5px;color:#666666;font-size:17px">
-                                                                        The prices you will receive are very low due our
+                                                                        The prices you will receive are very low due to
                                                                         network of transport providers already
                                                                         travelling similar routes so they can
                                                                         conveniently carry out your delivery
